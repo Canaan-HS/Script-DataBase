472dd4f34e3385f617dbc7e0842bdd9835d2cd92
TenshinoOtoKafu <s3459897@gmail.com>
2025-03-02 00:59:06 +0800

    移動

--- Mapping/Cleaning.js
+++
@@ -1,9 +0,0 @@
-import path from "path";
-import { fileURLToPath } from "url";
-process.chdir(path.dirname(fileURLToPath(import.meta.url)));
-
-import { File } from "../File.mjs";
-
-File.Read("./List.json", true).then(List => {
-    File.Write(List, "./List.json");
-})
