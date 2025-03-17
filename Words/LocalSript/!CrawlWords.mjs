583f8fa327cf14455c467faaf6f4fbf1cc3835c4
TenshinoOtoKafu <s3459897@gmail.com>
2025-03-18 05:39:54 +0800

    本地處理腳本

---
+++ Words/LocalSript/!CrawlWords.mjs
@@ -0,0 +1,119 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+process.chdir(path.dirname(fileURLToPath(import.meta.url)));
+
+import axios from 'axios';
+import * as marked from 'marked';
+import * as cheerio from 'cheerio';
+import * as OpenCC from "opencc-js";
+import { File } from '../../File.mjs';
+
+/**
+ * 解析 Csv 數據轉換成 Json 輸出, Data 需要傳遞 array
+ */
+async function CsvToJson(Data) {
+    let Read_Data = "";
+
+    for (const Path of Data) { // 讀出來的是字串, 將所有字串合併
+        Read_Data += await File.Read(`${Path}.csv`, false);
+    };
+
+    const ParseBox = {};
+    for (const str of Read_Data.split("\r\n")) {
+        for (const str1 of str.replace(/"/g, "").split(",")) {
+            if (/^\d+$/.test(str1)) continue;
+            ParseBox[str1.replace(/_/g, " ")] = "";
+        }
+    }
+
+    File.Write(ParseBox, "Csv.json");
+}
+
+/**
+ * 用於快速獲取 Wiki 角色列表的範本
+ */
+function PrintWikiCharacterList() {
+    const data = {};
+    document.querySelectorAll("dl dt").forEach(item => {
+        const match = item.textContent.match(/^(.+?)\s*（.*?，\s*([\w\s]+)）/);
+        if (match) {
+            data[match[2].toLowerCase()] = match[1];
+        }
+    });
+
+    console.log(JSON.stringify(data, null, 2));
+}
+
+/**
+ * 該函數用於抓取: https://github.com/EhTagTranslation/Database
+ * 
+ * 讀取本地字典, 獲取更新的部份
+ */
+async function DataCrawl(Data) {
+    // 獲取本地已存在 字典
+    const Exclude = await File.Read("../Exclude.json");
+    const All_Words = await File.Read("../All_Words.json");
+    const CNtoTW = OpenCC.Converter({ from: "cn", to: "tw" });
+
+    const localKeys = new Set( // 將全部字典 和 排除的字典 合併, 取得所有 key 值
+        Object.keys(Object.assign(All_Words, Exclude))
+    );
+
+    for (const data of Data) {
+        const NewData = {}; // 緩存遠端新字典
+        const {Name, Uri} = data; // 解構傳遞的數據
+
+        for (const type of Uri) { // 取得數據類型
+            try {
+                const remote = await axios.get(`https://raw.githubusercontent.com/EhTagTranslation/Database/master/database/${type}.md`);
+                const html = marked.marked(remote.data); // 將 md 數據解析為 html
+                const $ = cheerio.load(html); // 使用 cheerio 解析 HTML 內容
+
+                // 跳過第一個查找 遍歷所有 tr
+                const dict = {};
+                $("tbody tr").slice(1).each((_, tr) => {
+                    const td = $(tr).find("td"); // 從 tr 中取出所有 td
+
+                    const key = $(td[0]).text().trim();
+                    const value = $(td[1]).text().trim();
+
+                    if (key && value) {
+
+                        if (
+                            !/^\d+$/.test(key) // 排除 key 都是數字
+                            && key.length > 2 // 排除 key 長度小於 2
+                            && key.toLowerCase() != value.toLowerCase() // 排除 key 和 value 相同
+                        ) {
+                            const noSpaceValue = value.replace(" | ", "|");
+                            dict[key] = Name == "Group" ? noSpaceValue : CNtoTW(noSpaceValue); // 轉換繁體 (Group 會有日文不轉換)
+                        }
+                    }
+                });
+
+                // 過濾已經擁有的 localKeys
+                const filtered = Object.fromEntries(
+                    Object.entries(dict).filter(([key]) => !localKeys.has(key))
+                );
+
+                // 最後合併到新字典
+                Object.assign(NewData, filtered);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        // 將新數據輸出到, 緩存硬碟
+        if (Object.keys(NewData).length > 0) {
+            File.Write(NewData, `R:/New_${Name}.json`);
+        } else {
+            console.log(`${Name} 無新數據`);
+        }
+    }
+}
+
+DataCrawl([
+    {Name: "Character", Uri: ["character"]},
+    {Name: "Parody", Uri: ["parody"]},
+    {Name: "Group", Uri: ["group"]},
+    {Name: "Tags", Uri: ["other", "mixed", "male", "female"]},
+]);
---
+++ Words/LocalSript/!WordsGenerator.mjs
@@ -0,0 +1,146 @@
+import path from "path";
+import { fileURLToPath } from "url";
+process.chdir(path.dirname(fileURLToPath(import.meta.url)));
+
+import { File } from "../../File.mjs";
+
+/**
+ * @param {Array} Data - 數據需要是 ["json1", "json2"...] 的格式
+ * @param {boolean} Sort - 是否進行排序
+ * @param {boolean} Merge - 是否合併輸出 (合併輸出就會是 All_Words.json)
+ * @param {boolean} LengthSort - 是否使用長度來排序, 否的話使用 字母
+ * @param {boolean} SimilarExcl - 排除 key 和 value 類似的, 會另外輸出類似的
+ */
+async function DataCleaning({
+    Data,
+    Sort=true,
+    Merge=false,
+    LengthSort=true,
+    SimilarExcl=false,
+}={}) {
+    const Read_Data = {}, Similar = {};
+
+    for (const Path of Data) { // 讀取所有傳入的數據
+        Read_Data[Path] = await File.Read(`../${Path}.json`);
+    };
+
+    if (Merge) {
+        let Cache = Sort ? [] : {};
+        const ProcessedData = Object.assign(...Object.values(Read_Data)); // 將全部物件合併
+        for (const [Key, Value] of Object.entries(ProcessedData)) {
+
+            const Cleaning = CleaningTreatment(Key, Value);
+            if (!Cleaning) continue;
+            const [Clean_key, Clean_value] = Cleaning;
+
+            Sort
+                ? Cache.push({
+                    data: { [Clean_key]: Clean_value },
+                    length: (Clean_key + Clean_value).length,
+                })
+                : Cache[Clean_key] = Clean_value;
+        };
+
+        if (Sort) Cache = SortBy(Cache);
+        SimilarExcl && OutputSimilar();
+        File.Write(Cache, "../All_Words.json"); // 輸出文件
+
+        return true; // 完成回傳
+    } else {
+        const Split_Box = {};
+        for (const [Key, Value] of Object.entries(Read_Data)) {
+            let Cache = Sort ? [] : {};
+
+            for (const [V_Key, V_Value] of Object.entries(Value)) {
+
+                const Cleaning = CleaningTreatment(V_Key, V_Value);
+                if (!Cleaning) continue;
+                const [Clean_key, Clean_value] = Cleaning;
+
+                Sort
+                    ? Cache.push({
+                        data: { [Clean_key]: Clean_value },
+                        length: (Clean_key + Clean_value).length,
+                    })
+                    : Cache[Clean_key] = Clean_value;
+            }
+
+            if (Sort) Cache = SortBy(Cache);
+            Split_Box[Key] = Cache;
+        };
+
+        // 無合併的數據, 會進行比對, 由後傳入的覆蓋先傳入的
+        const All_key = Object.keys(Split_Box);
+        let key_Index = All_key.length - 1;
+
+        for (; key_Index >= 0; key_Index--) { // 後像依序像前面比較, 並刪除前面重複的值
+            const compareKeys = Object.keys(Split_Box[All_key[key_Index]]);
+            for (let Comp_Index = key_Index - 1; Comp_Index >= 0; Comp_Index--) {
+                for (const key of compareKeys) {
+                    delete Split_Box[All_key[Comp_Index]][key];
+                }
+            }
+        };
+
+        // 最後分別輸出
+        SimilarExcl && OutputSimilar();
+        for (const [key, value] of Object.entries(Split_Box)) {
+            File.Write(value, `../${key}.json`);
+        }
+
+        return true; // 完成回傳
+    }
+
+    // 輸出類似
+    function OutputSimilar() {
+        if (Object.keys(Similar).length > 0) File.Write(Similar, `Similar.json`);
+    };
+
+    // 清潔方式
+    function CleaningTreatment(Key, Value) {
+        if (/^\d+$/.test(Key)) return; // key 值都是數字的排除
+        const [clean_key, clean_value] = [Key.trim().toLowerCase(), Value.trim()]; // 清潔數據格式
+        if (SimilarExcl) {
+            const [similar_key, similar_value] = [
+                Key.replace(/[\W_]+/g, ""),
+                Value.toLowerCase().replace(/[\W_]+/g, "")
+            ];
+            if (similar_key == similar_value) { // 對相似的進行排除
+                Similar[clean_key] = clean_value;
+                return;
+            }
+        }
+        return [clean_key, clean_value];
+    };
+
+    // 排序方式
+    function SortBy(Cache) {
+        LengthSort
+            ? Cache.sort((a, b) => a.length - b.length) // 長度排序
+            : Cache.sort((a, b) => Object.keys(a.data)[0].localeCompare(Object.keys(b.data)[0])); // 單字排序
+
+        const Result = {};
+        Cache.forEach(item => {
+            Object.assign(Result, item.data);
+        })
+
+        return Result;
+    };
+};
+
+/* ======================================================= */
+
+// 後傳入的優先級越高
+// DataCleaning({
+    // Data: ["!Exclude"]
+// })
+
+DataCleaning({ // 個別處理
+    LengthSort: false,
+    Data: ["Beautify", "Group", "Artist", "Parody", "Character", "Short", "Long", "Language", "Tags"]
+}).then(()=> {
+    DataCleaning({ // 整合輸出
+        Merge: true,
+        Data: ["Beautify", "Group", "Artist", "Parody", "Character", "Short", "Long", "Language", "Tags"]
+    })
+});
